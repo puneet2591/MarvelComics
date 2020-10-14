f82cf756395762c4f5be2a216113566c95eed624
Puneet Mahajan <puneetonly12@gmail.com>
2020-10-14 21:01:37 +0530

    Initial Commit

---
+++ MarvelComics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelComics
+//
+//  Created by Puneet Mahajan on 14/10/20.
+//  Copyright © 2020 Puneet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
